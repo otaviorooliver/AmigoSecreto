3ce9cf02349aa5611b9ddddf29865fd3a5ff99cf
Otávio Rodrigues <otavio.rooliver@gmail.com>
2025-09-19 23:09:20 -0300

    Função atualizar amigos

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
-
+let listaExibir = document.getElementById('listaAmigos');
 
 function limparCampo() {
-    chute = document.querySelector('input');
-    chute.value = '';
+    entrada = document.querySelector('input');
+    entrada.value = '';
+}
+
+function atualizarLista (){
+    // console.log(listaExibir);
+    listaExibir.innerHTML = "";
+
+    for(var i = 0; i < listaAmigos.length; i++){
+        let elemento = document.createElement('li');
+        elemento.textContent = listaAmigos[i];
+        listaExibir.appendChild(elemento);
+        // console.log(listaExibir);
+    }
 }
 
 function adicionarAmigo() {
@@ -16,6 +28,18 @@ function adicionarAmigo() {
         listaAmigos.push(amigo);
         console.log(listaAmigos);
         limparCampo();
+        atualizarLista();
     }
 }
+
+// function sortearAmigo(){
+//     if (listaAmigos = 0){
+//         alert('Insira um nome antes de sortear.')
+//     } else{
+//         let numero = Math.floor(Math.random()* 10);
+//         console.log(numero)
+//     }
+
+// }
+
 
